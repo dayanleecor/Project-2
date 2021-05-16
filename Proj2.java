dd32b780ddb6186bdd56d978dcb3c266eafbb044
dayanleecor <48634004+dayanleecor@users.noreply.github.com>
2021-05-16 16:37:50 -0400

    Add files via upload

---
+++ Proj2.java
@@ -0,0 +1,14 @@
+//import proj2;
+import proj2.Table;
+
+public class Proj2 {
+
+	/**
+	* Runs the whole project.
+	*/
+	public static void main(String args[])
+	{
+		Table t = new Table();
+		t.setVisible(true);
+	}
+}	
